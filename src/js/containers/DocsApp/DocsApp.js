7936a6916f756192319c30f9e0139b4d2bfc8084
derekja <derekja@gmail.com>
2016-03-19 19:02:00 -0700

    clean up docsapp

--- src/js/containers/DocsApp/DocsApp.js
+++ src/js/containers/DocsApp/DocsApp.js
@@ -3,31 +3,11 @@ import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'reac
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Subdivide, { reducer as subdivide } from 'subdivide';
-import marktex from 'marktex';
-//import mathjax from 'mathjax';
+
+
 
 const ACTIVE = { color: 'red' }
-/*
-var txt = '**inline math**: $mr\omega^2 = F$, and math block. $$$ f(a) = \frac{1}{2\pi i}\oint_\gamma \frac{f(z)  }{z-a}dz $$$';
 
-var options = {
-  gfm: true,
-  marktex: true,
-  math: function(mathString, isInline, language){
-            return isInline ? '<span class="mathjax">\\('+mathString+'\\)</span>'
-            :'<div class="mathjax">\\['+mathString+'\\]</div>';
-            }
-};
-
-var foo = 'test';
-
-  
-marktex(txt, options, function (err, content) {
-  if (err) throw err;
-  console.log(content);
-  foo =content;
-});
-*/
 class DocsApp extends Component {
  constructor(props, ctx) {
     super(props, ctx)
