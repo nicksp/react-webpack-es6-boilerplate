6547468aca20f2daf6293d462393ec0d480d6bb4
André Perdigão <andrepcg@gmail.com>
2016-04-28 18:24:50 +0100

    Add support to Windows

---
+++ bin/clean.js
@@ -0,0 +1,12 @@
+var rimraf = require('rimraf');
+var fs = require('fs');
+
+var dir = '../build';
+
+rimraf(dir, function(err){
+	if(err)
+		throw err;
+	
+	if(process.argv.indexOf("createdir") >= 0)
+		fs.mkdirSync(dir);
+});
