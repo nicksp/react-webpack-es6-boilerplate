23c92ef1bd0769e147a32be3b3f91cfb5a60e055
Lutando Ngqakaza <lutando@ngqakaza.com>
2017-02-27 15:13:15 +0200

    Using browser router

--- src/client/assets/javascripts/app/App.js
+++ src/client/assets/javascripts/app/App.js
@@ -1,14 +1,22 @@
 import React, { PropTypes } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import FriendsView from 'features/friends/components/FriendsView';
+import NotFound from 'components/NotFound';
 
 const App = (props) => (
+<BrowserRouter
+ onChange={(location) => {
+   console.log(location)
+ }}
+  >
   <div className="page-container">
-    { /*React.cloneElement({...props}.children, {...props}) */}
-
+    <Switch>
+      <Route exact path='/' component={FriendsView}/>
+      <Route exact path='/404' component={NotFound} />
+      <Redirect from="/*" to='/404' />
+    </Switch>
   </div>
+</BrowserRouter>
 );
-
-App.propTypes = {
-  //children: PropTypes.element.isRequired
-};
 
 export default App;
